ab6df936672a8cb9ff1f20f4c38712614ee9f7ff
dhruvinparikh <imsilent93@gmail.com>
2021-08-10 18:07:25 -0400

    updates

--- notes/dapp-development/demo/survey-marketplace-hardhat/tasks/deploy-surveyfactory.js
+++ notes/dapp-development/demo/survey-marketplace-hardhat/tasks/deploy-surveyfactory.js
@@ -1,29 +1,61 @@
 const { task } = require("hardhat/config");
+const { boolean } = require("hardhat/internal/core/params/argumentTypes");
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 task("deploy-surveyfactory", "deploys the survey factory contract")
-  .addParam("signeraddress", "address of the signer")
+  .addOptionalParam("signeraddress", "address of the signer")
   .addParam("surveycreationfees", "survey creation fees in ETH")
-  .setAction(async ({ signeraddress, surveycreationfees }, hre) => {
-    
+  .addParam("verify", "verify the contract on etherscan",false,boolean)
+  .setAction(async ({ signeraddress, surveycreationfees, verify }, hre) => {
     // await hre.run("accounts");
-
-    const signer = await hre.ethers.getSigner(
-      hre.ethers.utils.getAddress(signeraddress)
+    const fees = hre.ethers.utils.parseUnits(
+      surveycreationfees.toString(),
+      (unit = "ether")
     );
 
-    const fees = hre.ethers.utils.parseUnits( surveycreationfees.toString() ,unit = "ether") 
-
     const SurveyFactory = await ethers.getContractFactory("SurveyFactory");
-    const surveyFactory = await SurveyFactory.connect(signer).deploy(fees);
-    await surveyFactory.deployed();
+    let surveyFactory;
 
+    console.log('Migration started\n');
+    console.log('1. Deploy survey factory');
+    if (
+      signeraddress == "" ||
+      signeraddress == undefined ||
+      signeraddress == null
+    ) {
+      surveyFactory = await SurveyFactory.deploy(fees);
+    } else {
+      const signer = await hre.ethers.getSigner(
+        hre.ethers.utils.getAddress(signeraddress)
+      );
+      surveyFactory = await SurveyFactory.connect(signer).deploy(fees);
+    }
+
+    await surveyFactory.deployed();
 
     const initSurveyCreationFees = await surveyFactory.surveyCreationFees();
 
     const surveyFactoryOwner = await surveyFactory.owner();
 
-    console.log("Survey Factory Contract Address : ", surveyFactory.address)
-    console.log("Survey creation fees in ETH : ", hre.ethers.utils.formatEther(initSurveyCreationFees.toString()))
-    console.log("Survey Factory owner : ", surveyFactoryOwner)
+    if (verify) {
+      console.log('2. Verifying contracts');
+      await delay(3000)
+      await hre.run("verify:verify", {
+        address: surveyFactory.address,
+        constructorArguments: [
+          fees.toString()
+        ],
+      });
+    }
 
+    console.log("Survey Factory Contract Address : ", surveyFactory.address);
+    console.log(
+      "Survey creation fees in ETH : ",
+      hre.ethers.utils.formatEther(initSurveyCreationFees.toString())
+    );
+    console.log("Survey Factory owner : ", surveyFactoryOwner);
+    console.log('\nFinished migrations');
   });
