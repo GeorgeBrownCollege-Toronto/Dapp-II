3749e01e9cb486b44f503a80141f19a7823f1d12
Dhruvin Parikh <29928916+dhruvinparikh@users.noreply.github.com>
2021-08-10 19:25:48 -0400

    Refactor test case (#47)
    * updates
    
    * run testcase# 1
    
    * display account balances pre-post deployment

--- notes/dapp-development/demo/survey-marketplace-hardhat/scripts/deploy.js
+++ notes/dapp-development/demo/survey-marketplace-hardhat/scripts/deploy.js
@@ -5,13 +5,14 @@ async function main() {
 
     console.log("Deloying contracts with the accounts :", deployer.address);
 
-    console.log("Account balance:", (await deployer.getBalance()).toString());
+    console.log("Account balance before contract deployment:", (await deployer.getBalance()).toString());
 
     const SurveyFactory = await ethers.getContractFactory("SurveyFactory");
     const surveyFactory = await SurveyFactory.deploy("1000000000");
     await surveyFactory.deployed();
 
     console.log("Survey Factory address :", surveyFactory.address);
+    console.log("Account balance after contract deployment:", (await deployer.getBalance()).toString());
 }
 
 main().then(() => process.exit(0)).catch(error => {
