02dfa1030c5bca83b642bf0516a8593e01a02776
Dhruvin Parikh <29928916+dhruvinparikh@users.noreply.github.com>
2021-08-19 17:40:28 -0400

    Day4 (#56)
    * updates
    
    * update index
    
    * updates to fe

--- notes/dapp-development/demo/survey-marketplace-frontend/hooks/useCreateSurvey.ts
+++ notes/dapp-development/demo/survey-marketplace-frontend/hooks/useCreateSurvey.ts
@@ -1,11 +1,13 @@
 import { Contract } from "@ethersproject/contracts"
 import { useContract } from './useContract';
 import SurveyFactory from "../contracts/SurveyFactory.json"
+// import { TokenAmount } from '@uniswap/sdk'
+import { BigNumberish } from "@ethersproject/bignumber"
 
-export function useCreateSurvey(suspense = false): any {
+export function useCreateSurvey(fee: BigNumberish, suspense = false): any {
     const contract = useContract(SurveyFactory?.address, SurveyFactory?.abi, true);
 
     return async () => {
-        return (contract as Contract).createSurvey({value:"1100000000", gasLimit:"3000000"});
-    } 
+        return (contract as Contract).createSurvey({ value: fee, gasLimit: "3000000" });
+    }
 }
